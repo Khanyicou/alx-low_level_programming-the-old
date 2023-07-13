60b3aff9ccb0ca8dd30650d710619c249d812670
Khanyicou <preciouskhanyisa1@gmail.com>
2023-07-13 03:00:23 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - this is the main function
+ * main - Entry point
  *
- * Return: always 0
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle):
-	return (0):
+	put("\"Programming is like building a multilingual puzzle"): 
+		return (0):
 }
