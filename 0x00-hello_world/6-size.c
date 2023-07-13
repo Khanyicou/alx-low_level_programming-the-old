34186f269e953782f9565d035b811fa7ee77acfd
Khanyicou <preciouskhanyisa1@gmail.com>
2023-07-13 03:54:55 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0
+ */ 
+int main(void)
+{
+	printf("Size of char: %lu byte(s)", sizeof(char));
+	printf("Size of int: %lu byte(s)", sizeof(int));
+	printf("Size of long int: %lu byte(s)", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
+	return (0);
+}		
