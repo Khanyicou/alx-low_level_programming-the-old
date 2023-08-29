41e471e700173014c32fdb6e4c65303167a4e362
Khanyicou <preciouskhanyisa1@gmail.com>
2023-08-29 15:52:05 -0700

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,46 +2,47 @@
 
 /**
  * insert_nodeint_at_index - Inserts a new node at a given position.
- * @head: Pointer to a pointer to the head of the list.
- * @idx: Index where the new node should be added.
- * @n: Data to be stored in the new node.
+ * @head: Pointer to the head of the list.
+ * @idx: new node should be added.
+ * @n: Data to be stored
  *
- * Return: Address of the new node, or NULL if it failed.
+ * Return: Address of the new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 {
-	 unsigned int i;
-	 listint_t *new_node, *current;
 
-	 if (head == NULL)
-		 return (NULL);
+listint_t *new_node, *current;
+unsigned int i;
 
-	 new_node = malloc(sizeof(listint_t));
-	 if (new_node == NULL)
-		 return (NULL);
+if (head == NULL)
+return (NULL);
 
-	 new_node->n = n;
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
 
-	 if (idx == 0)
-	{
-		*head = new_node;
-		new_node->next = *head;
-		return (new_node);
-	}
+new_node->n = n;
 
-	 current = *head;
-	 for (i = 0; current != NULL && i < idx - 1; i++)
-		 current = current->next;
+if (idx == 0)
+{
+new_node->next = *head;
+*head = new_node;
+return (new_node);
+}
 
-	 if (current == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+current = *head;
+for (i = 0; current != NULL && i < idx - 1; i++)
+current = current->next;
 
-	 new_node->next = current->next;
-	 current->next = new_node;
+if (current == NULL)
+{
+free(new_node);
+return (NULL);
+}
 
-	 return new_node;
+new_node->next = current->next;
+current->next = new_node;
+
+return (new_node);
 
 }
