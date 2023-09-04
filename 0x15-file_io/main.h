eab2120ad55e87af2b93db058930235e42200fd8
Khanyicou <preciouskhanyisa1@gmail.com>
2023-09-04 12:03:46 -0700

    update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,11 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcnti.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int _putchar(char c);
